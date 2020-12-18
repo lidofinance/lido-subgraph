9abb8e28cefc221d987d039cc8deb7655030ccf6
KRogLA <i@krogla.me>
2020-12-18 19:49:11 +0100

    mainnet update

---
+++ build_and_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./.env
+set -e +u
+set -o pipefail
+
+TAG=${TAG:-"latest"}
+IMG="lidofinance/lido-subgraph"
+export DOCKER_CONFIG=$HOME/.lidofinance
+
+echo "Building $IMG:$TAG Docker image..."
+docker build -t $IMG:$TAG .
+
+echo "Pushing $IMG:$TAG to the Docker Hub"
+# docker push $IMG:$TAG
