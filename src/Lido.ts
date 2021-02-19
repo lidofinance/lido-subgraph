aa648ebdaf310da3a04f6b50dd344ec91e01a250
kolyasapphire <a@ks.gg>
2021-02-19 12:54:30 +0300

    Match ABI order

--- src/Lido.ts
+++ src/Lido.ts
@@ -1,80 +1,42 @@
 import {
+  Stopped,
+  Resumed,
+  Transfer,
+  Approval,
   FeeSet,
   FeeDistributionSet,
   Submitted,
-  Withdrawal,
   Unbuffered,
-  Transfer,
-  Approval,
-  Stopped,
-  Resumed,
+  Withdrawal,
 } from "../generated/Lido/Lido";
 import {
-  LidoSubmission,
-  LidoFee,
-  LidoFeeDistribution,
-  LidoWithdrawal,
-  LidoUnbuffered,
-  LidoTransfer,
-  LidoApproval,
   LidoStopped,
   LidoResumed,
+  LidoTransfer,
+  LidoApproval,
+  LidoFee,
+  LidoFeeDistribution,
+  LidoSubmission,
+  LidoUnbuffered,
+  LidoWithdrawal,
 } from "../generated/schema";
 
-export function handleSubmit(event: Submitted): void {
-  let entity = new LidoSubmission(
+export function handleStopped(event: Stopped): void {
+  let entity = new LidoStopped(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
-  entity.sender = event.params.sender;
-  entity.amount = event.params.amount;
-  entity.referral = event.params.referral;
+  entity.blocktime = event.block.timestamp;
 
   entity.save();
 }
 
-export function handleFeeSet(event: FeeSet): void {
-  let entity = new LidoFee(
+export function handleResumed(event: Resumed): void {
+  let entity = new LidoResumed(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
-  entity.feeBasisPoints = event.params.feeBasisPoints;
-
-  entity.save();
-}
-
-export function handleFeeDistributionSet(event: FeeDistributionSet): void {
-  let entity = new LidoFeeDistribution(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
-
-  entity.treasuryFeeBasisPoints = event.params.treasuryFeeBasisPoints;
-  entity.insuranceFeeBasisPoints = event.params.insuranceFeeBasisPoints;
-  entity.operatorsFeeBasisPoints = event.params.operatorsFeeBasisPoints;
-
-  entity.save();
-}
-
-export function handleWithdrawal(event: Withdrawal): void {
-  let entity = new LidoWithdrawal(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
-
-  entity.sender = event.params.sender;
-  entity.tokenAmount = event.params.tokenAmount;
-  entity.sentFromBuffer = event.params.sentFromBuffer;
-  entity.pubkeyHash = event.params.pubkeyHash;
-  entity.etherAmount = event.params.etherAmount;
-
-  entity.save();
-}
-
-export function handleUnbuffered(event: Unbuffered): void {
-  let entity = new LidoUnbuffered(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
-
-  entity.amount = event.params.amount;
+  entity.blocktime = event.block.timestamp;
 
   entity.save();
 }
@@ -103,22 +65,60 @@ export function handleApproval(event: Approval): void {
   entity.save();
 }
 
-export function handleStopped(event: Stopped): void {
-  let entity = new LidoStopped(
+export function handleFeeSet(event: FeeSet): void {
+  let entity = new LidoFee(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
-  entity.blocktime = event.block.timestamp;
+  entity.feeBasisPoints = event.params.feeBasisPoints;
 
   entity.save();
 }
 
-export function handleResumed(event: Resumed): void {
-  let entity = new LidoResumed(
+export function handleFeeDistributionSet(event: FeeDistributionSet): void {
+  let entity = new LidoFeeDistribution(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
-  entity.blocktime = event.block.timestamp;
+  entity.treasuryFeeBasisPoints = event.params.treasuryFeeBasisPoints;
+  entity.insuranceFeeBasisPoints = event.params.insuranceFeeBasisPoints;
+  entity.operatorsFeeBasisPoints = event.params.operatorsFeeBasisPoints;
+
+  entity.save();
+}
+
+export function handleSubmit(event: Submitted): void {
+  let entity = new LidoSubmission(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  entity.sender = event.params.sender;
+  entity.amount = event.params.amount;
+  entity.referral = event.params.referral;
+
+  entity.save();
+}
+
+export function handleUnbuffered(event: Unbuffered): void {
+  let entity = new LidoUnbuffered(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  entity.amount = event.params.amount;
+
+  entity.save();
+}
+
+export function handleWithdrawal(event: Withdrawal): void {
+  let entity = new LidoWithdrawal(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  entity.sender = event.params.sender;
+  entity.tokenAmount = event.params.tokenAmount;
+  entity.sentFromBuffer = event.params.sentFromBuffer;
+  entity.pubkeyHash = event.params.pubkeyHash;
+  entity.etherAmount = event.params.etherAmount;
 
   entity.save();
 }
