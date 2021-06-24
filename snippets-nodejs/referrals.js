e8cfc58a78cc1b6bc509be666439f95824cd7f2d
kolyasapphire <a@ks.gg>
2021-06-24 14:39:49 +0300

    Total referred and unique addresses example

--- snippets-nodejs/referrals.js
+++ snippets-nodejs/referrals.js
@@ -1,9 +1,12 @@
 const fetcher = require('./helpers/fetcher')
 
+const Big = require('big.js')
+
 const referral = '0x1234'
 
 const query = `query {
 	lidoSubmissions(where: {referral:"${referral}"}) {
+    sender
 	  amount
   }
 }`
@@ -11,11 +14,17 @@ const query = `query {
 const getTotalAddressReferral = async () => {
   const submissions = (await fetcher(query)).lidoSubmissions
 
-  const total = submissions.reduce(
-    (acc, item) => acc + parseInt(item.amount),
-    0
+  const total = submissions.reduce((acc, item) => acc.plus(item.amount), Big(0))
+
+  const uniqueReferred = [...new Set(submissions.map((x) => x.sender))]
+
+  console.log(
+    referral,
+    'referred a total of',
+    total.div(1e18).round(1).toNumber(),
+    'stETH'
   )
-  console.log('This referrer ID referred a total of:', total)
+  console.log(referral, 'referred', uniqueReferred.length, 'unique addresses')
 }
 
 getTotalAddressReferral()
