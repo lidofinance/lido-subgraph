a7f9c0e5815201b69f09a6b1a80c2e68a91f8772
kolyasapphire <a@ks.gg>
2021-07-01 15:31:07 +0300

    Minor referral example improvement

--- snippets-nodejs/referrals.js
+++ snippets-nodejs/referrals.js
@@ -2,15 +2,20 @@ const fetcher = require('./helpers/fetcher')
 
 const Big = require('big.js')
 
+const WEI = Big('1e18')
+const HUMAN_DECIMALS = 2
+
 const referral = '0x1234'
 
 const query = `query {
-	lidoSubmissions(where: {referral:"${referral}"}) {
+	lidoSubmissions(where: {referral:"${referral}"}, orderBy: blockTime, orderDirection: desc) {
     sender
 	  amount
+    blockTime
   }
 }`
 
+// This example is stats-only, doesn't take limits into account
 const getTotalAddressReferral = async () => {
   const submissions = (await fetcher(query)).lidoSubmissions
 
@@ -21,7 +26,7 @@ const getTotalAddressReferral = async () => {
   console.log(
     referral,
     'referred a total of',
-    total.div(1e18).round(1).toNumber(),
+    total.div(WEI).round(HUMAN_DECIMALS).toNumber(),
     'stETH'
   )
   console.log(referral, 'referred', uniqueReferred.length, 'unique addresses')
