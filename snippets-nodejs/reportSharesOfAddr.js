967a63994febe427057a2971941b2b3a31e5015c
kolyasapphire <a@ks.gg>
2021-08-26 17:30:14 +0300

    Shares on each report example

---
+++ snippets-nodejs/reportSharesOfAddr.js
@@ -0,0 +1,38 @@
+const fetcher = require('./helpers/fetcher')
+const Web3 = require('web3')
+const fs = require('fs')
+const Big = require('big.js')
+
+const LIDO_ADDRESS = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'
+const ABI = JSON.parse(fs.readFileSync('./../abis/Lido.json'))
+
+// Make sure to use an archive node!
+const rpc = 'http://RPC'
+const address = process.env.ADDRESS
+
+const web3 = new Web3(rpc)
+const contract = new web3.eth.Contract(ABI, LIDO_ADDRESS)
+const contractFunc = contract.methods.sharesOf(address)
+
+const query = `
+query {
+	  oracleCompleteds (first: 1000, orderBy: block, orderDirection: asc) {
+		block
+		blockTime
+	  }
+	}
+`
+
+const reportSharesOfAddr = async () => {
+  const oracleReports = (await fetcher(query)).oracleCompleteds
+
+  for (let report of oracleReports) {
+    const balance = await contractFunc.call({}, report.block)
+    const humanTime = new Date(report.blockTime * 1000).toLocaleDateString(
+      'ru-RU'
+    )
+    console.log(humanTime, balance)
+  }
+}
+
+reportSharesOfAddr()
