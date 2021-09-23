4b95f86e320dbaef3ec002fb04a24f7b6b6d3247
kolyasapphire <a@ks.gg>
2021-09-23 12:57:03 +0300

    Negative tx values test

---
+++ tests/noTxNegativeValues.test.js
@@ -0,0 +1,18 @@
+import { subgraphFetch, gql } from './utils'
+
+const query = gql`
+  query {
+    lidoTransfers(
+      first: 100000
+      where: { sharesAfterDecrease_lt: 0, balanceAfterDecrease_lt: 0 }
+    ) {
+      id
+    }
+  }
+`
+
+test('there are no transactions going to minus', async () => {
+  const lidoTransfers = (await subgraphFetch(query)).lidoTransfers
+
+  expect(lidoTransfers).toEqual([])
+})
