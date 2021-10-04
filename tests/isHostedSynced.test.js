41a6537d7a21a73216445c8785f6610d14c95452
kolyasapphire <a@ks.gg>
2021-10-04 14:34:07 +0300

    Increased hosted isSynced test block check

--- tests/isHostedSynced.test.js
+++ tests/isHostedSynced.test.js
@@ -2,7 +2,7 @@ import { ethCall, getHostedLastIndexedBlock } from './utils'
 
 test('isHostedSynced', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
-  const acceptedMinimum = currentBlock - 100
+  const acceptedMinimum = currentBlock - 1000 // ~ 3.5 hours of blocks
 
   const subgraphBlock = parseInt(await getHostedLastIndexedBlock())
 
