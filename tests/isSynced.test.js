d3cd00e5ba2f8cbdd5e5f60f3d3766cd799b433e
kolyasapphire <a@ks.gg>
2021-11-01 17:17:28 +0300

    Sync test less strict

--- tests/isSynced.test.js
+++ tests/isSynced.test.js
@@ -1,4 +1,9 @@
+import { jest } from '@jest/globals'
 import { ethCall, getLastIndexedBlock } from './utils'
+
+jest.setTimeout(20000) // 1 second
+
+jest.retryTimes(3)
 
 test('isSynced', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
