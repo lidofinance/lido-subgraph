cc0249f7d96707f1eacd6045170246fdab16d9f6
kolyasapphire <a@ks.gg>
2021-12-11 16:53:57 +0300

    Test for oracle completeds instead of rewards

--- tests/allOracleReportsExist.test.js
+++ tests/allOracleReportsExist.test.js
@@ -2,7 +2,7 @@ import { getOracleEventNumber, getEntityCount } from './utils'
 
 test('allOracleReportsExist', async () => {
   const ethNumber = await getOracleEventNumber('Completed')
-  const subgraphNumber = await getEntityCount('totalRewards')
+  const subgraphNumber = await getEntityCount('oracleCompleteds')
 
   expect(subgraphNumber).toEqual(ethNumber)
 })
