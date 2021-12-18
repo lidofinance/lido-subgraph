968bb2829aa84f833797768bf157a4c7f6835cfe
kolyasapphire <a@ks.gg>
2021-12-18 16:08:52 +0300

    Allowing more desync on isSynced test

--- tests/isSynced.test.js
+++ tests/isSynced.test.js
@@ -1,13 +1,13 @@
 import { jest } from '@jest/globals'
 import { ethCall, getLastIndexedBlock } from './utils'
 
-jest.setTimeout(20000) // 1 second
+jest.setTimeout(20000)
 
-jest.retryTimes(3)
+jest.retryTimes(10)
 
 test('isSynced', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
-  const acceptedMinimum = currentBlock - 3
+  const acceptedMinimum = currentBlock - 5
 
   const subgraphBlock = parseInt(await getLastIndexedBlock())
 
