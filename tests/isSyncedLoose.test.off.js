3015b0d9a1b7e3b4b5c6411561638dfc9bd8df12
kolyasapphire <a@ks.gg>
2021-12-21 13:20:59 +0300

    Loose sync test for monitoring

--- tests/isSyncedLoose.test.off.js
+++ tests/isSyncedLoose.test.off.js
@@ -1,13 +1,13 @@
 import { jest } from '@jest/globals'
 import { ethCall, getLastIndexedBlock } from './utils'
 
-jest.setTimeout(10000)
+jest.setTimeout(20000)
 
-jest.retryTimes(3)
+jest.retryTimes(10)
 
-test('isSynced', async () => {
+test('isSyncedLoose', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
-  const acceptedMinimum = currentBlock - 2
+  const acceptedMinimum = currentBlock - 10
 
   const subgraphBlock = parseInt(await getLastIndexedBlock())
 
