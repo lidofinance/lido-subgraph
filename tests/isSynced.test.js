2c246593efd1ab46959f930c59026d0f0c164c63
kolyasapphire <a@ks.gg>
2022-03-14 17:32:53 +0300

    Sync test loose by default

--- tests/isSynced.test.js
+++ tests/isSynced.test.js
@@ -3,11 +3,11 @@ import { ethCall, getLastIndexedBlock } from './utils/index.js'
 
 jest.setTimeout(20000)
 
-jest.retryTimes(20)
+jest.retryTimes(3)
 
-test('isSynced', async () => {
+test('isSyncedLoose', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
-  const acceptedMinimum = currentBlock - 2
+  const acceptedMinimum = currentBlock - 50
 
   const subgraphBlock = parseInt(await getLastIndexedBlock())
 
--- tests/isSyncedStrict.test.off.js
+++ tests/isSyncedStrict.test.off.js
@@ -3,11 +3,11 @@ import { ethCall, getLastIndexedBlock } from './utils/index.js'
 
 jest.setTimeout(20000)
 
-jest.retryTimes(3)
+jest.retryTimes(20)
 
-test('isSyncedLoose', async () => {
+test('isSynced', async () => {
   const currentBlock = parseInt((await ethCall('getBlock', 'latest')).number)
-  const acceptedMinimum = currentBlock - 100
+  const acceptedMinimum = currentBlock - 2
 
   const subgraphBlock = parseInt(await getLastIndexedBlock())
 
