4f4982ad35507e294cec51f2a9093b4fff6bf42d
kolyasapphire <a@ks.gg>
2022-03-15 15:28:39 +0300

    Subgraph fixed block requests

--- tests/utils/subgraphFetch.js
+++ tests/utils/subgraphFetch.js
@@ -1,5 +1,15 @@
 import { request } from 'graphql-request'
-import { GRAPH, GRAPH_MONITORING } from '../config.js'
+import { GRAPH, GRAPH_MONITORING, RPC_SYNC_BLOCK } from '../config.js'
+
+const adjustQuery = (query) => {
+  if (!RPC_SYNC_BLOCK) {
+    return query
+  }
+
+  return query.replace(')', `, block: { number: ${process.env.BLOCK} })`)
+}
 
 export const subgraphFetch = async (query, monitoring = false) =>
-  await request(monitoring ? GRAPH_MONITORING : GRAPH, query)
+  !monitoring
+    ? await request(GRAPH, adjustQuery(query))
+    : await request(GRAPH_MONITORING, query)
