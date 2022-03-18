820c8510dae1dacc284991be4fd4de8d13c136f0
kolyasapphire <a@ks.gg>
2022-03-18 18:26:09 +0300

    Simple reward history example from subgraph data

--- snippets-nodejs/utils/Big.js
+++ snippets-nodejs/utils/Big.js
@@ -1,8 +1,17 @@
 import Big from 'big.js'
 
-Big.RM = 0
-Big.DP = 0
-Big.NE = -10000
-Big.PE = 10000
+// Separate instance for displaying ETH
+const BigDecimal = Big()
 
-export { Big }
+Big.DP = 0
+BigDecimal.DP = 18
+
+Big.RM = Big.roundDown
+BigDecimal.RM = Big.roundHalfUp
+
+Big.NE = -70000000
+BigDecimal.NE = -70000000
+Big.PE = 210000000
+BigDecimal.PE = 210000000
+
+export { Big, BigDecimal }
