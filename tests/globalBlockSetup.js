b99f3610ce96fd059f34a0041a7178301b50b8cb
kolyasapphire <a@ks.gg>
2022-05-24 22:35:56 +0300

    test: allow missing env vars with a warning

--- tests/globalBlockSetup.js
+++ tests/globalBlockSetup.js
@@ -1,6 +1,15 @@
+import { GRAPH, RPC } from './config.js'
 import { getLastIndexedBlock, getRpcNetwork } from './utils/index.js'
 
-process.env.BLOCK = await getLastIndexedBlock()
+if (GRAPH) {
+  process.env.BLOCK = await getLastIndexedBlock()
+} else {
+  console.info('BLOCK env was not set as there is no GRAPH provided.')
+}
 
-const networkName = (await getRpcNetwork()).name
-process.env.NETWORK = networkName === 'homestead' ? 'mainnet' : networkName
+if (RPC) {
+  const networkName = (await getRpcNetwork()).name
+  process.env.NETWORK = networkName === 'homestead' ? 'mainnet' : networkName
+} else {
+  console.info('NETWORK env was not set as there is no RPC provided.')
+}
