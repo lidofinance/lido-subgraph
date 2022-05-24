49eafa92fe87baf050830bddba3cb4cb1086ee23
kolyasapphire <a@ks.gg>
2022-05-24 20:42:57 +0300

    test: RPC request helpers refactor

--- tests/utils/ethCalls.js
+++ tests/utils/ethCalls.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 import fs from 'fs'
 
-import { RPC, LIDO_ADDRESS, RPC_SYNC_BLOCK } from '../config.js'
+import { RPC, LIDO_ADDRESS, getBlock } from '../config.js'
 
 const provider = new ethers.providers.JsonRpcProvider(RPC)
 
@@ -12,28 +12,28 @@ const oracleAddress = await lidoContract.getOracle()
 const oracleAbi = JSON.parse(fs.readFileSync('abis/LidoOracle.json'))
 const oracleContract = new ethers.Contract(oracleAddress, oracleAbi, provider)
 
-const genCallBlock = async (args) => {
+const mbAddBlock = async (args) => {
   const blockIsOverriden = args.find((x) => x.blockTag)
 
   if (blockIsOverriden) {
     return args
   }
 
-  if (RPC_SYNC_BLOCK) {
-    args.push({ blockTag: parseInt(process.env.BLOCK) })
-  }
+  const block = getBlock()
+
+  args.push({ blockTag: block })
 
   return args
 }
 
 export const ethCall = async (func, ...initialArgs) =>
-  await provider[func](...(await genCallBlock(initialArgs)))
+  await provider[func](...(await mbAddBlock(initialArgs)))
 
 export const lidoFuncCall = async (func, ...initialArgs) =>
-  await lidoContract[func](...(await genCallBlock(initialArgs)))
+  await lidoContract[func](...(await mbAddBlock(initialArgs)))
 
 export const oracleFuncCall = async (func, ...initialArgs) =>
-  await oracleContract[func](...(await genCallBlock(initialArgs)))
+  await oracleContract[func](...(await mbAddBlock(initialArgs)))
 
 export const getAddressShares = async (address, ...args) =>
   await lidoFuncCall('sharesOf', address, ...args)
@@ -44,19 +44,18 @@ export const getAddressBalance = async (address, ...args) =>
 export const getBalanceFromShares = async (address, ...args) =>
   await lidoFuncCall('getPooledEthByShares', address, ...args)
 
-const genEventBlock = async () =>
-  RPC_SYNC_BLOCK ? parseInt(process.env.BLOCK) : 'latest'
-
 export const getLidoEventNumber = async (eventName) => {
   const filter = lidoContract.filters[eventName]()
-  const logs = lidoContract.queryFilter(filter, 0, await genEventBlock())
+  const logs = lidoContract.queryFilter(filter, 0, getBlock())
 
   return (await logs).length
 }
 
 export const getOracleEventNumber = async (eventName) => {
   const filter = oracleContract.filters[eventName]()
-  const logs = oracleContract.queryFilter(filter, 0, await genEventBlock())
+  const logs = oracleContract.queryFilter(filter, 0, getBlock())
 
   return (await logs).length
 }
+
+export const getRpcNetwork = async () => await provider.getNetwork()
