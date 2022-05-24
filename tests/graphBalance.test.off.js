1045d91af871ab85b91a57ba93a66e5da0a523d0
kolyasapphire <a@ks.gg>
2022-05-24 16:21:29 +0300

    test: tries for Graph balance

--- tests/graphBalance.test.off.js
+++ tests/graphBalance.test.off.js
@@ -1,9 +1,14 @@
 import fs from 'fs'
 import ethers from 'ethers'
+import { jest } from '@jest/globals'
 
 const BILLING_CONTRACT_ADDRESS = '0x10829DB618E6F520Fa3A01c75bC6dDf8722fA9fE'
 const LIDO_ADDRESS = process.env.THEGRAPH_BILLING_ADDRESS
 const THRESHOLD_ETH = 1 * 1000 // 1k GRT
+
+jest.setTimeout(10000)
+
+jest.retryTimes(3)
 
 test('The Graph balance check', async () => {
   const provider = new ethers.providers.JsonRpcProvider(
