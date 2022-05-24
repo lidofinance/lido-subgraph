7086f534454a056813525377837a45e69e0c9261
kolyasapphire <a@ks.gg>
2022-05-24 20:45:59 +0300

    test: dust checks rework

--- tests/noWrongDust.test.js
+++ tests/noWrongDust.test.js
@@ -1,21 +1,43 @@
 import { gql } from 'graphql-request'
 import { subgraphFetch } from './utils/index.js'
 
-// If we chose a wrong dust boundary, dust can be mistaken as treasury fee
-const query = gql`
-  query ($first: Int, $skip: Int) {
+const dustNullQuery = gql`
+  query ($first: Int, $skip: Int, $block: Block_height) {
     totalRewards(
       first: $first
       skip: $skip
+      block: $block
+      where: { dust: null }
+    ) {
+      id
+      block
+    }
+  }
+`
+
+test('there is no rewards with null dust', async () => {
+  const totalRewards = (await subgraphFetch(dustNullQuery)).totalRewards
+
+  expect(totalRewards).toEqual([])
+})
+
+// If we chose a wrong dust boundary, dust can be mistaken as treasury fee
+const dustBoundaryQuery = gql`
+  query ($first: Int, $skip: Int, $block: Block_height) {
+    totalRewards(
+      first: $first
+      skip: $skip
+      block: $block
       where: { dust: 0, treasuryFee_not: null }
     ) {
-      dust
+      id
+      block
     }
   }
 `
 
 test('there is no mismatched dust transactions', async () => {
-  const totalRewards = (await subgraphFetch(query)).totalRewards
+  const totalRewards = (await subgraphFetch(dustBoundaryQuery)).totalRewards
 
   expect(totalRewards).toEqual([])
 })
