312be9ab9d06f560a1e24e2253a78bd46c823231
kolyasapphire <a@ks.gg>
2022-05-24 20:41:06 +0300

    refactor: fetcher rework for tests

--- tests/utils/subgraphFetch.js
+++ tests/utils/subgraphFetch.js
@@ -1,15 +1,55 @@
 import { request } from 'graphql-request'
-import { GRAPH, GRAPH_MONITORING, RPC_SYNC_BLOCK } from '../config.js'
+import { GRAPH, getBlock } from '../config.js'
 
-const adjustQuery = (query) => {
-  if (!RPC_SYNC_BLOCK) {
+const FETCH_STEP = 1000
+const SKIP_STEP = 1000
+
+// Add fixed block if not set already
+const mbAddFixedBlock = (vars) => {
+  if (vars.block) {
     return query
   }
 
-  return query.replace(')', `, block: { number: ${process.env.BLOCK} })`)
+  const block = getBlock()
+  return { ...vars, block: { number: block } }
 }
 
-export const subgraphFetch = async (query, monitoring = false) =>
-  !monitoring
-    ? await request(GRAPH, adjustQuery(query))
-    : await request(GRAPH_MONITORING, query)
+// Warning: Assumes one-key objects
+const mergeObjects = (array) =>
+  array.reduce((acc, cur) => {
+    const key = Object.keys(cur)[0]
+    const exists = key === Object.keys(acc)[0]
+
+    acc[key] = exists ? [...acc[key], ...cur[key]] : cur[key]
+
+    return acc
+  }, {})
+
+export const subgraphFetch = async (query, vars = {}) => {
+  let skip = 0
+  let results = null
+
+  do {
+    const res = await request(GRAPH, query, {
+      first: FETCH_STEP,
+      skip: skip,
+      ...mbAddFixedBlock(vars),
+    })
+
+    results = results ? mergeObjects([results, res]) : res
+
+    // Exit if we don't need to pull all available data
+    if (vars?.first || vars?.skip) {
+      break
+    }
+
+    skip += SKIP_STEP
+  } while (
+    // Items should exist at all
+    results[Object.keys(results)[0]].length &&
+    // More items should exist
+    results[Object.keys(results)[0]].length % SKIP_STEP === 0
+  )
+
+  return results
+}
