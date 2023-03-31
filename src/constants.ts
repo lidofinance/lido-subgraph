5a53b2326fa5e66b56850d1f17e2b0231789fcd6
KRogLA <i@krogla.me>
2023-03-31 10:41:47 +0200

    fix: flow switchers

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import { BigInt, Address, TypedMap } from '@graphprotocol/graph-ts'
+import { BigInt, Address, TypedMap, ethereum } from '@graphprotocol/graph-ts'
 import { dataSource } from '@graphprotocol/graph-ts'
 
 import { Settings } from '../generated/schema'
@@ -35,6 +35,26 @@ export const ORACLE_RUNS_BUFFER = BigInt.fromI32(50)
 export const MAINNET_FIRST_ORACLE_REPORT = BigInt.fromI32(1610016625) // block 11607098
 export const TESTNET_FIRST_ORACLE_REPORT = BigInt.fromI32(1617282681) // block 4543056
 
+// upgrade to TransferShares event, block
+// mainnet: 14860268 (https://etherscan.io/tx/0x11a48020ae69cf08bd063f1fbc8ecf65bd057015aaa991bf507dbc598aadb68e)
+// "transactionIndex": "0x10b"
+export const MAINNET_TRANSFER_SHARES_BLOCK = BigInt.fromI32(14860268)
+export const MAINNET_TRANSFER_SHARES_TX_INDEX = BigInt.fromI32(267)
+// goerli: 6912872 (https://goerli.etherscan.io/tx/0x61fdb6110874916557acdc51b039d0b12570675693375e8dfb4a24929d0bea45)
+// "transactionIndex": "0x2"
+export const TESTNET_TRANSFER_SHARES_BLOCK = BigInt.fromI32(6912872)
+export const TESTNET_TRANSFER_SHARES_TX_INDEX = BigInt.fromI32(2)
+
+// upgrade to Lido v2, block
+// mainnet: 14860268 (https://etherscan.io/tx/)
+// "transactionIndex": ""
+export const MAINNET_LIDO_V2_BLOCK = BigInt.fromI32(0)
+export const MAINNET_LIDO_V2_TX_INDEX = BigInt.fromI32(0)
+// goerli: 8710746 (https://goerli.etherscan.io/tx/0x75dae29ccd81f0b93c2207935e6c0e484ee6ad5307455015c962c9206ce7e8d6)
+// "transactionIndex": "0x48"
+export const TESTNET_LIDO_V2_BLOCK = BigInt.fromI32(8710746)
+export const TESTNET_LIDO_V2_TX_INDEX = BigInt.fromI32(72)
+
 // Oracle report period is dependent on network (eg much often on testnet)
 export const MAINNET_ORACLE_PERIOD = BigInt.fromI32(86400) // 1 day
 export const TESTNET_ORACLE_PERIOD = BigInt.fromI32(3840) // 10 epochs by ~6.4 minutes
@@ -44,6 +64,20 @@ export const getFirstOracleReport = (): BigInt =>
 
 export const getOraclePeriod = (): BigInt =>
   isMainnet ? MAINNET_ORACLE_PERIOD : TESTNET_ORACLE_PERIOD
+
+export const isTransferSharesUpgrade = (event: ethereum.Event): boolean =>
+  isMainnet
+    ? event.block.number >= MAINNET_TRANSFER_SHARES_BLOCK &&
+      event.transaction.index >= MAINNET_TRANSFER_SHARES_TX_INDEX
+    : event.block.number >= TESTNET_TRANSFER_SHARES_BLOCK &&
+      event.transaction.index > TESTNET_TRANSFER_SHARES_TX_INDEX
+
+export const isLidoV2Upgrade = (event: ethereum.Event): boolean =>
+  isMainnet
+    ? event.block.number >= MAINNET_LIDO_V2_BLOCK &&
+      event.transaction.index >= MAINNET_LIDO_V2_TX_INDEX
+    : event.block.number >= TESTNET_LIDO_V2_BLOCK &&
+      event.transaction.index > TESTNET_LIDO_V2_TX_INDEX
 
 /**
 Addresses
