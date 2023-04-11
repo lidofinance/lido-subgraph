ec26c94a85c70e49a67211963b1e2605adc19f06
KRogLA <i@krogla.me>
2023-04-11 12:15:53 +0200

    fix: scheme, helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,8 @@
 import { store, Bytes, ethereum, BigInt } from '@graphprotocol/graph-ts'
 import {
-  Totals,
-  Shares,
-  Stats,
+  Total,
+  Share,
+  Stat,
   LidoTransfer,
   TotalReward,
   Holder,
@@ -20,6 +20,8 @@ import {
   isAppVerMatch
 } from './constants'
 import { Transfer, TransferShares } from '../generated/Lido/Lido'
+
+
 
 export function _loadOrCreateLidoTransferEntity(
   eventTransfer: Transfer,
@@ -43,6 +45,16 @@ export function _loadOrCreateLidoTransferEntity(
 
     entity.value = eventTransfer.params.value
     entity.shares = eventTransferShares.params.sharesValue
+    entity.totalPooledEther = ZERO
+    entity.totalShares = ZERO
+
+    entity.balanceAfterDecrease = ZERO
+    entity.balanceAfterIncrease = ZERO
+    entity.sharesBeforeDecrease = ZERO
+    entity.sharesAfterDecrease = ZERO
+    entity.sharesBeforeDecrease = ZERO
+    entity.sharesAfterIncrease = ZERO
+    entity.mintWithoutSubmission = false
   }
   return entity
 }
@@ -100,10 +112,10 @@ export function _loadOrCreateTotalRewardEntity(
   return entity
 }
 
-export function _loadOrCreateStatsEntity(): Stats {
-  let stats = Stats.load('')
+export function _loadOrCreateStatsEntity(): Stat {
+  let stats = Stat.load('')
   if (!stats) {
-    stats = new Stats('')
+    stats = new Stat('')
     stats.uniqueHolders = ZERO
     stats.uniqueAnytimeHolders = ZERO
     stats.lastOracleCompletedId = ZERO
@@ -112,96 +124,98 @@ export function _loadOrCreateStatsEntity(): Stats {
   return stats
 }
 
-export function _loadOrCreateTotalsEntity(): Totals {
-  let totals = Totals.load('')
+export function _loadOrCreateTotalsEntity(): Total {
+  let totals = Total.load('')
   if (!totals) {
-    totals = new Totals('')
+    totals = new Total('')
     totals.totalPooledEther = ZERO
     totals.totalShares = ZERO
   }
   return totals
 }
 
-export function _loadOrCreateSharesEntity(id: Bytes): Shares {
-  let sharesEntity = Shares.load(id)
+export function _loadOrCreateSharesEntity(id: Bytes): Share {
+  let sharesEntity = Share.load(id)
   if (!sharesEntity) {
-    sharesEntity = new Shares(id)
+    sharesEntity = new Share(id)
     sharesEntity.shares = ZERO
   }
   return sharesEntity
 }
 
+export function _updateTransferBalances(entity: LidoTransfer): void {
+  if (entity.totalShares.isZero()) {
+    entity.balanceAfterIncrease = entity.value
+    entity.balanceAfterDecrease = ZERO
+  } else {
+    entity.balanceAfterIncrease = entity
+      .sharesAfterIncrease!.times(entity.totalPooledEther)
+      .div(entity.totalShares)
+    entity.balanceAfterDecrease = entity
+      .sharesAfterDecrease!.times(entity.totalPooledEther)
+      .div(entity.totalShares)
+  }
+}
+
 export function _updateTransferShares(entity: LidoTransfer): void {
-  // No point in changing 0x0 shares
-  if (!entity.shares.isZero()) {
-    // Decreasing from address shares
-    if (entity.from != ZERO_ADDRESS) {
-      // Address must already have shares, HOWEVER:
-      // Someone can and managed to produce events of 0 to 0 transfers
-      let sharesFromEntity = _loadOrCreateSharesEntity(entity.from)
+  // Increasing to address shares
+  if (entity.to != ZERO_ADDRESS) {
+    const sharesToEntity = _loadOrCreateSharesEntity(entity.to)
+    entity.sharesBeforeIncrease = sharesToEntity.shares
+    sharesToEntity.shares = sharesToEntity.shares.plus(entity.shares)
+    entity.sharesAfterIncrease = sharesToEntity.shares
+    sharesToEntity.save()
+  }
 
-      entity.sharesBeforeDecrease = sharesFromEntity.shares
-      sharesFromEntity.shares = sharesFromEntity.shares.minus(entity.shares)
-      entity.sharesAfterDecrease = sharesFromEntity.shares
-
-      sharesFromEntity.save()
-
-      // Calculating new balance
-      entity.balanceAfterDecrease = entity
-        .sharesAfterDecrease!.times(entity.totalPooledEther)
-        .div(entity.totalShares)
-    }
-
-    // Increasing to address shares
-    if (entity.to != ZERO_ADDRESS) {
-      let sharesToEntity = _loadOrCreateSharesEntity(entity.to)
-
-      entity.sharesBeforeIncrease = sharesToEntity.shares
-      sharesToEntity.shares = sharesToEntity.shares.plus(entity.shares)
-      entity.sharesAfterIncrease = sharesToEntity.shares
-
-      sharesToEntity.save()
-
-      // Calculating new balance
-      entity.balanceAfterIncrease = entity
-        .sharesAfterIncrease!.times(entity.totalPooledEther)
-        .div(entity.totalShares)
-    }
+  // Decreasing from address shares
+  if (entity.from != ZERO_ADDRESS) {
+    // Address must already have shares, HOWEVER:
+    // Someone can and managed to produce events of 0 to 0 transfers
+    const sharesFromEntity = _loadOrCreateSharesEntity(entity.from)
+    assert(sharesFromEntity.shares >= entity.shares)
+    entity.sharesBeforeDecrease = sharesFromEntity.shares
+    sharesFromEntity.shares = sharesFromEntity.shares.minus(entity.shares)
+    entity.sharesAfterDecrease = sharesFromEntity.shares
+    sharesFromEntity.save()
   }
 }
 
 export function _updateHolders(entity: LidoTransfer): void {
   // Saving recipient address as a unique stETH holder
-  if (!entity.shares.isZero()) {
-    let stats = _loadOrCreateStatsEntity()
-    let isNewHolder = false
-    let holder: Holder | null
-    // skip zero destination for any case
-    if (entity.to != ZERO_ADDRESS) {
-      holder = Holder.load(entity.to)
-      isNewHolder = !holder
-      if (isNewHolder) {
-        holder = new Holder(entity.to)
-        holder.address = entity.to
-        holder.save()
-      }
-    }
+  const stats = _loadOrCreateStatsEntity()
 
-    if (isNewHolder) {
-      stats.uniqueHolders = stats.uniqueHolders.plus(ONE)
+  // skip zero destination for any case
+  if (entity.to != ZERO_ADDRESS && !entity.balanceAfterIncrease!.isZero()) {
+    let holder = Holder.load(entity.to)
+    if (!holder) {
+      holder = new Holder(entity.to)
+      // @todo remove
+      holder.address = entity.to
+      holder.balance = ZERO
+
       stats.uniqueAnytimeHolders = stats.uniqueAnytimeHolders.plus(ONE)
-    } else if (
-      entity.from != ZERO_ADDRESS &&
-      entity.sharesAfterDecrease!.isZero()
-    ) {
-      // Mints don't have balanceAfterDecrease
-      stats.uniqueHolders = stats.uniqueHolders.minus(ONE)
+    }
+    if (holder.balance.isZero()) {
+      stats.uniqueHolders = stats.uniqueHolders.plus(ONE)
+    }
+    holder.balance = entity.balanceAfterIncrease!
+    holder.save()
+  }
+
+  if (entity.from != ZERO_ADDRESS) {
+    const holder = Holder.load(entity.from)
+    if (holder) {
+      if (!holder.balance.isZero() && entity.balanceAfterDecrease!.isZero()) {
+        stats.uniqueHolders = stats.uniqueHolders.minus(ONE)
+      }
+      holder.balance = entity.balanceAfterDecrease!
+      holder.save()
       // @todo delete holder
       // @todo check id correctness
       // store.remove('Holder', entity.from.toString())
-    }
-    stats.save()
+    } // else should not be
   }
+  stats.save()
 }
 
 // export function _loadOrCreateCounters(): Counters {
@@ -213,7 +227,6 @@ export function _updateHolders(entity: LidoTransfer): void {
 //   }
 //   return counters
 // }
-
 
 export const checkAppVer = (appId: Bytes, minUpgId: i32): bool => {
   const appVer = AppVersion.load(appId)
