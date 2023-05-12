04d04392fae52ce0dcad7f42015877024cb92169
KRogLA <i@krogla.me>
2023-05-12 17:06:18 +0200

    fix: typo

--- tests/graphBalance.test.off.alt.js
+++ tests/graphBalance.test.off.alt.js
@@ -1,7 +1,7 @@
 import { providers, Contract, constants } from 'ethers'
 import { jest } from '@jest/globals'
 import { getContractAddress } from './config.js'
-import bollingAbi from '../abis/Billing.json'
+import billingAbi from '../abis/Billing.json'
 
 const BILLING_CONTRACT_ADDRESS = '0x1B07D3344188908Fb6DEcEac381f3eE63C48477a'
 const THRESHOLD_ETH = constants.WeiPerEther.mul(1 * 1000) // 1k GRT
@@ -11,7 +11,7 @@ jest.retryTimes(3)
 
 test('The Graph balance check', async () => {
   const provider = new providers.JsonRpcProvider('https://arb1.arbitrum.io/rpc')
-  const contract = new Contract(BILLING_CONTRACT_ADDRESS, bollingAbi, provider)
+  const contract = new Contract(BILLING_CONTRACT_ADDRESS, billingAbi, provider)
   const balance = await contract.userBalances(getContractAddress('Lido'))
 
   expect(balance.gte(THRESHOLD_ETH)).toBe(true)
