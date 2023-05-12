7b8ab5d7cbde2e78dc29935085dbbaf7280d7539
KRogLA <i@krogla.me>
2023-05-12 13:22:47 +0200

    refactor: get addresses from networks.json

--- snippets-nodejs/config.js
+++ snippets-nodejs/config.js
@@ -7,4 +7,3 @@ export const ADDRESS = process.env.ADDRESS
 export const RPC = process.env.RPC
 export const GRAPH = process.env.GRAPH
 export const GRAPH_MONITORING = process.env.GRAPH_MONITORING
-export const LIDO_ADDRESS = process.env.LIDO_ADDRESS
--- tests/config.js
+++ tests/config.js
@@ -1,19 +1,33 @@
+import networks from '../networks.json'
 export const RPC = process.env.RPC
 export const GRAPH = process.env.GRAPH
 export const GRAPH_BASIC_AUTH_USER = process.env.GRAPH_BASIC_AUTH_USER || ''
 export const GRAPH_BASIC_AUTH_PASSWORD =
   process.env.GRAPH_BASIC_AUTH_PASSWORD || ''
 export const GRAPH_AUTH_COOKIE = process.env.GRAPH_AUTH_COOKIE || ''
-export const LIDO_ADDRESS = process.env.LIDO_ADDRESS
-export const NOP_ADDRESS = process.env.NOP_ADDRESS
-export const ARAGON_ADDRESS = process.env.ARAGON_ADDRESS
-export const EASYTRACK_ADDRESS = process.env.EASYTRACK_ADDRESS
 /**
 Timeout is for testnet - needs to be adjusted for mainnet or when transaction count increases.
 **/
 export const RPC_TIMEOUT = process.env.RPC_TIMEOUT || 60 * 1000
 
 export const getBlock = () => parseInt(process.env.BLOCK)
-export const getNetwork = () => process.env.NETWORK
+export const getNetwork = () => process.env.NETWORK || ''
+export const getRpcBlock = () => parseInt(process.env.RPC_BLOCK)
 export const getIsMainnet = () => getNetwork() === 'mainnet'
 export const getIsLimited = () => process.env.LIMITED === 'true'
+
+export const getContractAddress = (name = '') => {
+  const network = getNetwork()
+  if (!networks[network]) {
+    throw new Error(`No contracts defined for network '${network}'`)
+  }
+  const {
+    [name]: { address },
+  } = networks[network] || {}
+  if (!address) {
+    throw new Error(
+      `No address defined for contract '${name}' at network '${network}'`
+    )
+  }
+  return address
+}
