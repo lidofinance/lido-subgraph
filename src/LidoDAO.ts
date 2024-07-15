f5b6ef69bd1b56e67d5cd60c2c718bad121c12f4
Taras Alekhin <tarens1995@gmail.com>
2024-07-15 13:18:56 +0200

    fixed indexing for holesky wrong app

--- src/LidoDAO.ts
+++ src/LidoDAO.ts
@@ -22,12 +22,21 @@ export function handleSetApp(event: SetAppEvent): void {
       // updating only in case of a new contract address
       if (entity.impl != event.params.app) {
         const repo = AppRepo.bind(Address.fromString(repoAddr))
-        const latest = repo.getLatestForContractAddress(event.params.app)
-        const semVer = latest.getSemanticVersion()
+        // fix indexing, for wrong app set for Holesky
+        try {
+          const latest = repo.getLatestForContractAddress(event.params.app)
+          const semVer = latest.getSemanticVersion()
 
-        entity.major = semVer[0]
-        entity.minor = semVer[1]
-        entity.patch = semVer[2]
+          entity.major = semVer[0]
+          entity.minor = semVer[1]
+          entity.patch = semVer[2]
+        } catch (e) {
+          if (e.includes('REPO_INEXISTENT_VERSION')) {
+            entity.major = 0;
+            entity.minor = 0;
+            entity.patch = 0;
+          }
+        }
         entity.impl = event.params.app
 
         entity.block = event.block.number
